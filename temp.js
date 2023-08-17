f58f8f1e65e09c6f1b52a590aa7ee601074ab1ef
MHY <2102200586@qq.com>
2023-08-17 14:39:44 +0800

    creat temp

---
+++ temp.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         New hello
+// @namespace    http://666.com/
+// @version      0.1
+// @description  try to take over the world!
+// @author       mm
+// @match        *://*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=greasyfork.org
+// @licence      AGPL-3.0
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
